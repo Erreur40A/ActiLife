657658fad535ce31dba0a480c824431c29b05e93
alc4ml <moreladjalla@gmail.com>
2025-04-02 21:49:13 +0200

    premiere version de rappel medicament y compris ajout suppression des medicaments

---
+++ app/src/main/java/com/cgp/actilife/MedicamentAdapter.java
@@ -0,0 +1,54 @@
+package com.cgp.actilife;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MedicamentAdapter extends RecyclerView.Adapter<MedicamentAdapter.MedicamentViewHolder> {
+
+    private List<Medicament> medicamentList;
+
+    public MedicamentAdapter(List<Medicament> medicamentList) {
+        this.medicamentList = medicamentList;
+    }
+
+    @Override
+    public MedicamentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Crée une vue à partir du layout
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_medicament, parent, false);
+        return new MedicamentViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MedicamentViewHolder holder, int position) {
+        // Récupère le médicament à la position donnée
+        Medicament medicament = medicamentList.get(position);
+
+        // Lie les données du médicament aux vues du layout
+        holder.nomTextView.setText(medicament.getNom());
+        holder.typeTextView.setText("(" + medicament.getType() + ")");
+        holder.heureTextView.setText(medicament.getHeure());
+    }
+
+    @Override
+    public int getItemCount() {
+        // Retourne le nombre d'éléments dans la liste
+        return medicamentList.size();
+    }
+
+    public class MedicamentViewHolder extends RecyclerView.ViewHolder {
+        public TextView nomTextView, typeTextView, heureTextView;
+
+        public MedicamentViewHolder(View view) {
+            super(view);
+            nomTextView = view.findViewById(R.id.nomMedicament);
+            typeTextView = view.findViewById(R.id.typeMedicament);
+            heureTextView = view.findViewById(R.id.heureMedicament);
+        }
+    }
+}
